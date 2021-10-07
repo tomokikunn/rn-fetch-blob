84a202175d60ddd1c266d3009a25eed211594801
Supawit Werajindapong <65423932+tomokikunn@users.noreply.github.com>
2021-10-07 21:16:46 +0700

    fix require cycle

--- polyfill/XMLHttpRequest.js
+++ polyfill/XMLHttpRequest.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a MIT-style license that can be
 // found in the LICENSE file.
 
-import RNFetchBlob from '../index.js'
+//import RNFetchBlob from '../index.js'
 import XMLHttpRequestEventTarget from './XMLHttpRequestEventTarget.js'
 import Log from '../utils/log.js'
 import Blob from './Blob.js'
